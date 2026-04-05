936314beb2944b3eac8c79a39c53925962fd0e2f
kamar ezzamaine zouaid <kmrzed59@gmail.com>
2026-04-05 16:37:02 +0100

    fixing the closeing of the session

--- kmr/student/backend/actions/logout.php
+++ kmr/student/backend/actions/logout.php
@@ -2,6 +2,12 @@
 
 require_once __DIR__ . '/../includes/bootstrap.php';
 
+if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'POST') {
+	logout_user();
+	http_response_code(204);
+	exit;
+}
+
 logout_user();
 set_flash('success', 'Deconnexion effectuee.');
 redirect('../../pages/login.php');
--- kmr/student/index.php
+++ kmr/student/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/backend/includes/bootstrap.php';
 
 if (is_authenticated()) {
-    redirect('pages/cours.php');
+    logout_user();
 }
 
 require __DIR__ . '/home.html';
