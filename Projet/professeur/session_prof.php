5318149312af09187c5dbbf7a72179eae36c7eb8
abderrahim hamdi <elhamdiabderrahim8@gmail.com>
2026-06-23 11:31:06 +0200

    Fix: Explicitly clear old cookie, set SameSite=None and Secure=true

--- Projet/professeur/session_prof.php
+++ Projet/professeur/session_prof.php
@@ -8,15 +8,23 @@ if (session_status() === PHP_SESSION_NONE) {
     // Force settings via ini_set - works regardless of php.ini location
     ini_set('session.cookie_path', '/');
     ini_set('session.cookie_httponly', '1');
-    ini_set('session.cookie_samesite', 'Lax');
+    ini_set('session.cookie_secure', '1');
+    ini_set('session.cookie_samesite', 'None');
     ini_set('session.gc_maxlifetime', '7200');
     ini_set('session.use_strict_mode', '1');
+
+    // KILL THE OLD COOKIE PATH to prevent browser cookie conflicts!
+    if (isset($_COOKIE['PHPSESSID'])) {
+        setcookie('PHPSESSID', '', time() - 3600, '/Projet/professeur/');
+        setcookie('PHPSESSID', '', time() - 3600, '/Projet/professeur');
+    }
 
     session_set_cookie_params([
         'lifetime' => 0,
         'path'     => '/',
+        'secure'   => true,
         'httponly' => true,
-        'samesite' => 'Lax',
+        'samesite' => 'None',
     ]);
     session_start();
 }
